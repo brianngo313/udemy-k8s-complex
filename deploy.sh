e5abf6d503b615ccccd35e927ace5bc9f83801a1
Brian Ngo <brian.ngo313@gmail.com>
2020-10-21 09:27:10 +0800

    Build docker image with SHA tag

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t brianngo313/multi-client -f ./client/Dockerfile ./client
-docker build -t brianngo313/multi-server -f ./server/Dockerfile ./server
-docker build -t brianngo313/multi-worker -f ./worker/Dockerfile ./worker
-docker push brianngo313/multi-client
-docker push brianngo313/multi-server
-docker push brianngo313/multi-worker
+docker build -t brianngo313/multi-client:latest -t brianngo313/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t brianngo313/multi-server:latest -t brianngo313/multi-client:$SHA -f ./server/Dockerfile ./server
+docker build -t brianngo313/multi-worker:latest -t brianngo313/multi-client:$SHA -f ./worker/Dockerfile ./worker
+
+docker push brianngo313/multi-client:latest
+docker push brianngo313/multi-server:latest
+docker push brianngo313/multi-worker:latest
+
+docker push brianngo313/multi-client:$SHA
+docker push brianngo313/multi-server:$SHA
+docker push brianngo313/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=brianngo313/multi-client
-kubectl set image deployments/server-deployment server=brianngo313/multi-server
-kubectl set image deployments/worker-deployment worker=brianngo313/multi-worker
+kubectl set image deployments/client-deployment client=brianngo313/multi-client:$SHA
+kubectl set image deployments/server-deployment server=brianngo313/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=brianngo313/multi-worker:$SHA
